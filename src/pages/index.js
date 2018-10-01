c89f6979c729c669055e08b1026929ba55910bb1
Carlos DIAZ <c.diaz@wks-97.corp.dailymotion.com>
2018-10-01 17:44:22 +0200

    Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,19 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>React for Designers</h1>
-    <p>Learn how to build a modern site using React and the most efficient libraries to get your
-       site/product online. Get familiar with Grid CSS, animations, interactions, dynamic data 
-       with Contentful and deploying your site with Netlify.</p>
-    <Link to="/page-2/">Watch the video.</Link>
-    
-  </Layout>
+    <div>
+      <div className="Hero">
+       <div className="HeroGroup">
+          <h1>React for Designers</h1>
+          <p>Learn how to build a modern site using React and the most efficient libraries to get your
+          site/product online. Get familiar with Grid CSS, animations, interactions, dynamic data 
+          with Contentful and deploying your site with Netlify.</p>
+          <Link to="/page-2/">Watch the video</Link>
+       </div>
+      </div>
+    </div>
+      
+
 )
 
 export default IndexPage
